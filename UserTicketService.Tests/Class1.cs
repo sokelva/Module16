a73ced61ae97489ee1180f514584be896d39be38
elsokolova <elsokolova@korusconsulting.ru>
2023-03-20 13:14:18 +0300

    16.7.1. ПРАКТИКА (HW-03)

--- UserTicketService.Tests/Class1.cs
+++ UserTicketService.Tests/Class1.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Moq;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserTicketService;
 
 namespace UserTicketService.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class CalculatorTests
     {
+        [Test]
+        public void AddAlwaysReturnsExpectedValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Add(10, 220), Is.EqualTo(230));
+        }
     }
 }
--- UserTicketService/Program.cs
+++ UserTicketService/Program.cs
@@ -1,15 +1,80 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserTicketService
+namespace Practices
 {
-    internal class Program
+    public class Calculator
     {
-        static void Main(string[] args)
+        // Main Method
+        static public void Main(String[] args)
         {
+
+            Console.WriteLine("Main Method");
         }
+
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(100, 10) == 110);
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(100, 10) == 90);
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(100, 10) == 1000);
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(200, 10) == 20);
+        }
+
+        [TestFixture]
+        public class CalculatorTests
+        {
+            [Test]
+            public void Division_MustThrowException()
+            {
+                var calculator = new Calculator();
+                Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+            }
+        }
+
     }
 }
+
